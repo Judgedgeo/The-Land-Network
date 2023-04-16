701caaa212bfe9fae740db262f3d23b9a4b11995
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-04-16 18:27:14 -0500

    worked on user thought routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/user-controller');
+
+//API Users
+router.route('/').get(getUsers).post(createUser);
+
+//API Users/:UsersID
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+
+
+module.exports = router;
+
